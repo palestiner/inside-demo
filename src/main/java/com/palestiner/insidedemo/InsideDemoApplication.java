f56a6427ba457d9ee0386a6ed01902b37f0a3bb9
Yuri Medvedev <megapalestiner@gmail.com>
2022-06-30 03:16:13 +0700

    Initial commit

---
+++ src/main/java/com/palestiner/insidedemo/InsideDemoApplication.java
@@ -0,0 +1,13 @@
+package com.palestiner.insidedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsideDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsideDemoApplication.class, args);
+	}
+
+}
