d5d30315ac6445f55c62b32b1b3591035efaf086
Yuri Medvedev <megapalestiner@gmail.com>
2022-06-30 03:17:00 +0700

    added security dependencies

--- src/main/java/com/palestiner/insidedemo/InsideDemoApplication.java
+++ src/main/java/com/palestiner/insidedemo/InsideDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InsideDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InsideDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InsideDemoApplication.class, args);
+    }
 
 }
